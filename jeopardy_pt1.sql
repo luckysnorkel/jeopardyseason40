6c257d6e9791bd0b42ddbe169377bf01fd577e04
Katelyn Mueller <144558102+luckysnorkel@users.noreply.github.com>
2024-01-19 15:52:45 -0800

    Update jeopardy_pt1.sql
    Refined and corrected some SQL queries

--- jeopardy_pt1.sql
+++ jeopardy_pt1.sql
@@ -3,6 +3,20 @@ SELECT SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) AS CountCorrect,
 	ROUND(100.0 * SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) / COUNT(*),2) AS PercentCorrect
 FROM jeopardy_data;
 
+-- Average Responses Overall
+WITH AnswerBreakdown AS
+(SELECT Game,
+	SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) AS CorrectCount,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 1 ELSE 0 END) AS IncorrectCount,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN 1 ELSE 0 END) AS DNRCount
+FROM jeopardy_data
+GROUP BY Game
+ORDER BY Game)
+SELECT ROUND(AVG(CorrectCount),0) AS AverageCorrect,
+	ROUND(AVG(IncorrectCount),0) AS AverageIncorrect,
+	ROUND(AVG(DNRCount),0) AS AverageDNR
+FROM AnswerBreakdown;
+
 -- Overall percentage of correct by Round
 SELECT Round,
 	COUNT(*) AS Total,
@@ -10,6 +24,24 @@ SELECT Round,
 	ROUND(100.0 * SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) / COUNT(*),2) AS PercentCorrect
 FROM jeopardy_data
 GROUP BY Round;
+
+-- Average Responses By Round
+WITH AnswerBreakdown AS
+(SELECT Game, Round,
+	SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) AS CorrectCount,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 1 ELSE 0 END) AS IncorrectCount,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN 1 ELSE 0 END) AS DNRCount
+FROM jeopardy_data
+WHERE Round != 'Final Jeopardy!'
+GROUP BY Game, Round
+ORDER BY Game)
+SELECT Round,
+	ROUND(AVG(CorrectCount),0) AS AverageCorrect,
+	ROUND(AVG(IncorrectCount),0) AS AverageIncorrect,
+	ROUND(AVG(DNRCount),0) AS AverageDNR
+FROM AnswerBreakdown
+GROUP BY Round
+ORDER BY Round DESC;
 
 -- Count of questions by Metacategory
 SELECT Metacategory, 
@@ -21,16 +53,18 @@ ORDER BY QuestionCount DESC;
 -- Confidence & Win Rate by Metacategory
 SELECT Metacategory,
 	COUNT(*) AS QuestionCount,
-	ROUND(100.0 * SUM(CASE WHEN Response = 'Did Not Ring In' THEN 0 ELSE 1 END) / COUNT(*), 2) AS PercentAttempt,
-	ROUND(100.0 * SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) / COUNT(*), 2) AS PercentCorrect,
-	ROUND(100*(ROUND(100.0 * SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) / COUNT(*), 2) / ROUND(100.0 * SUM(CASE WHEN Response = 'Did Not Ring In' THEN 0 ELSE 1 END) / COUNT(*), 2)),2) AS PercentCorrectofAttempted
+	ROUND(100 * SUM(CASE WHEN Response = 'Did Not Ring In' THEN 0 ELSE 1 END) / COUNT(*), 2) AS PercentAttempt,
+	ROUND(100 * SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) / COUNT(*), 2) AS TotalPercentCorrect,
+	ROUND(100*(ROUND(100 * SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) / COUNT(*), 2) / ROUND(100 * SUM(CASE WHEN Response = 'Did Not Ring In' THEN 0 ELSE 1 END) / COUNT(*), 2)),2) AS PercentCorrectofAttempted
 FROM jeopardy_data
 GROUP BY Metacategory
-HAVING (ROUND(100.0 * SUM(CASE WHEN Response = 'Did Not Ring In' THEN 0 ELSE 1 END) / COUNT(*), 2)) != 0
-ORDER BY QuestionCount DESC, PercentCorrectofAttempted, PercentAttempt, PercentCorrect DESC;
+HAVING (ROUND(100 * SUM(CASE WHEN Response = 'Did Not Ring In' THEN 0 ELSE 1 END) / COUNT(*), 2)) != 0
+ORDER BY QuestionCount DESC, PercentAttempt, TotalPercentCorrect DESC;
 
--- Average Coryat
-SELECT ROUND(AVG(MyCoryat),0)
+-- Coryat Average and Ranges
+SELECT ROUND(AVG(MyCoryat),0) AS AverageCoryat,
+	MIN(MyCoryat) AS MinCoryat,
+	MAX(MyCoryat) AS MaxCoryat
 FROM jeopardy_coryats;
 
 -- Performance in DDs
@@ -45,7 +79,7 @@ ROUND(100.0 * SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) / COUNT(*), 
 FROM jeopardy_data
 WHERE DD = 'True'
 GROUP BY Metacategory
-ORDER BY total DESC, percentage DESC;
+ORDER BY total DESC, percentage;
 
 -- Performance in FJ
 SELECT COUNT(*) AS Total, SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) AS CorrectAnswers,
@@ -59,15 +93,7 @@ ROUND(100.0 * SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) / COUNT(*), 
 FROM jeopardy_data
 WHERE Round = 'Final Jeopardy!'
 GROUP BY Metacategory
-ORDER BY total DESC, percentage DESC;
-
--- Comparisons by game type
-SELECT b.GameType, 
-AVG(b.MyCoryat) AS AverageCoryat
-FROM jeopardy_data a
-JOIN jeopardy_coryats b 
-ON a.Game = b.Game
-GROUP BY b.GameType;
+ORDER BY total DESC, percentage;
 
 -- Correlation Between My Coryat Score and Performance
 SELECT a.Game, 
@@ -78,90 +104,144 @@ JOIN jeopardy_coryats b ON a.Game = b.Game
 GROUP BY a.Game, b.MyCoryat
 ORDER BY CorrectAnswers DESC;
 
--- My Coryat vs combined coryat
+---Metacategory breakdown by game
 SELECT Game,
-	MyCoryat,
-	CombinedCoryat
-FROM jeopardy_coryats
+	Metacategory,
+	COUNT(Response) AS TotalResponse,
+	SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) AS CorrectCount,
+	SUM(CASE WHEN Response = 'Correct' THEN Value ELSE 0 END) AS CorrectValue,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 1 ELSE 0 END) AS IncorrectCount,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 2*Value ELSE 0 END) AS IncorrectValue,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN 1 ELSE 0 END) AS DNRCount,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN Value ELSE 0 END) AS DNRValue
+FROM jeopardy_data
+GROUP BY Game, Metacategory
+ORDER BY Game, totalresponse DESC;
 
--- Correlation Between Contestant Coryat Score, FJ answer and Win
-SELECT Game,
-	CASE WHEN Contestant1Win = 'True' THEN Contestant1Coryat
-	WHEN Contestant2Win = 'True' THEN Contestant2Coryat
-	WHEN Contestant3Win = 'True' THEN Contestant3Coryat END AS WinningCoryat,
-	CASE WHEN Contestant1Win = 'True' THEN Contestant1FJ
-	WHEN Contestant2Win = 'True' THEN Contestant2FJ
-	WHEN Contestant3Win = 'True' THEN Contestant3FJ END AS WinnerFJ
-FROM jeopardy_coryats
+--Average total amount left on table per game
+WITH MetaValues AS (SELECT Game,
+	COUNT(Response) AS TotalResponse,
+	SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) AS CorrectCount,
+	SUM(CASE WHEN Response = 'Correct' THEN Value ELSE 0 END) AS CorrectValue,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 1 ELSE 0 END) AS IncorrectCount,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 2*Value ELSE 0 END) AS IncorrectValue,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN 1 ELSE 0 END) AS DNRCount,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN Value ELSE 0 END) AS DNRValue
+FROM jeopardy_data
+GROUP BY Game)
+SELECT ROUND(sum((IncorrectValue+DNRValue)/80),0) AS TotalLeft
+FROM MetaValues
 ;
 
 -- How much am I leaving on the table in History per game?
-WITH historyinc AS (
-SELECT Game,
-	SUM(2*Value) AS incvalue
+WITH MetaValuesHistory AS (SELECT Game,
+	COUNT(Response) AS TotalResponse,
+	SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) AS CorrectCount,
+	SUM(CASE WHEN Response = 'Correct' THEN Value ELSE 0 END) AS CorrectValue,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 1 ELSE 0 END) AS IncorrectCount,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 2*Value ELSE 0 END) AS IncorrectValue,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN 1 ELSE 0 END) AS DNRCount,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN Value ELSE 0 END) AS DNRValue
 FROM jeopardy_data
-WHERE Metacategory = 'History' AND Response = 'Incorrect'
-GROUP BY Game),
-historydnr AS (
-SELECT Game,
-	SUM(Value) AS dnrvalue
+WHERE Metacategory = 'History'
+GROUP BY Game)
+SELECT ROUND(SUM(IncorrectValue+DNRValue)/80,0) AS TotalLeft
+FROM MetaValuesHistory
+;
+
+-- How many History questions go unanswered or incorrectly answered per game?
+WITH MetaValuesHistory AS (SELECT Game,
+	COUNT(Response) AS TotalResponse,
+	SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) AS CorrectCount,
+	SUM(CASE WHEN Response = 'Correct' THEN Value ELSE 0 END) AS CorrectValue,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 1 ELSE 0 END) AS IncorrectCount,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 2*Value ELSE 0 END) AS IncorrectValue,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN 1 ELSE 0 END) AS DNRCount,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN Value ELSE 0 END) AS DNRValue
 FROM jeopardy_data
-WHERE Metacategory = 'History' AND Response = 'Did Not Ring In'
-GROUP BY Game),
-historylost AS (
-SELECT a.Game,
-	SUM(a.incvalue + b.dnrvalue) AS PotentialLostEarnings
-FROM historyinc a
-JOIN historydnr b
-ON a.Game = b.Game
-GROUP BY a.Game)
-SELECT ROUND(AVG(PotentialLostEarnings),0)
-FROM historylost;
+WHERE Metacategory = 'History'
+GROUP BY Game)
+SELECT ROUND(SUM(IncorrectCount+DNRCount)/80,0) AS TotalLeft
+FROM MetaValuesHistory
+;
 
 -- How much am I leaving on the table in Geography per game?
-WITH geographyinc AS (
-SELECT Game,
-	SUM(2*Value) AS incvalue
+WITH MetaValuesGeography AS (SELECT Game,
+	COUNT(Response) AS TotalResponse,
+	SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) AS CorrectCount,
+	SUM(CASE WHEN Response = 'Correct' THEN Value ELSE 0 END) AS CorrectValue,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 1 ELSE 0 END) AS IncorrectCount,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 2*Value ELSE 0 END) AS IncorrectValue,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN 1 ELSE 0 END) AS DNRCount,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN Value ELSE 0 END) AS DNRValue
 FROM jeopardy_data
-WHERE Metacategory = 'Geography' AND Response = 'Incorrect'
-GROUP BY Game),
-geographydnr AS (
-SELECT Game,
-	SUM(Value) AS dnrvalue
+WHERE Metacategory = 'Geography'
+GROUP BY Game)
+SELECT ROUND(SUM(IncorrectValue+DNRValue)/80,0) AS TotalLeft
+FROM MetaValuesGeography
+;
+
+-- How many Geography questions go unanswered or incorrectly answered per game?
+WITH MetaValuesGeography AS (SELECT Game,
+	COUNT(Response) AS TotalResponse,
+	SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) AS CorrectCount,
+	SUM(CASE WHEN Response = 'Correct' THEN Value ELSE 0 END) AS CorrectValue,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 1 ELSE 0 END) AS IncorrectCount,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 2*Value ELSE 0 END) AS IncorrectValue,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN 1 ELSE 0 END) AS DNRCount,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN Value ELSE 0 END) AS DNRValue
 FROM jeopardy_data
-WHERE Metacategory = 'Geography' AND Response = 'Did Not Ring In'
-GROUP BY Game),
-geographylost AS (
-SELECT a.Game,
-	SUM(a.incvalue + b.dnrvalue) AS PotentialLostEarnings
-FROM geographyinc a
-JOIN geographydnr b
-ON a.Game = b.Game
-GROUP BY a.Game)
-SELECT ROUND(AVG(PotentialLostEarnings),0)
-FROM geographylost;
+WHERE Metacategory = 'Geography'
+GROUP BY Game)
+SELECT ROUND(SUM(IncorrectCount+DNRCount)/80,0) AS TotalLeft
+FROM MetaValuesGeography
+;
+
+-- How much am I leavning on the table in Science per game?
+WITH MetaValuesScience AS (SELECT Game,
+	COUNT(Response) AS TotalResponse,
+	SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) AS CorrectCount,
+	SUM(CASE WHEN Response = 'Correct' THEN Value ELSE 0 END) AS CorrectValue,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 1 ELSE 0 END) AS IncorrectCount,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 2*Value ELSE 0 END) AS IncorrectValue,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN 1 ELSE 0 END) AS DNRCount,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN Value ELSE 0 END) AS DNRValue
+FROM jeopardy_data
+WHERE Metacategory = 'Science'
+GROUP BY Game)
+SELECT ROUND(SUM(IncorrectValue+DNRValue)/80,0) AS TotalLeft
+FROM MetaValuesScience
+;
 
 -- How much am I leaving on the table in Words per game?
-WITH wordsinc AS (
-SELECT Game,
-	SUM(2*Value) AS incvalue
+WITH MetaValuesWords AS (SELECT Game,
+	COUNT(Response) AS TotalResponse,
+	SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) AS CorrectCount,
+	SUM(CASE WHEN Response = 'Correct' THEN Value ELSE 0 END) AS CorrectValue,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 1 ELSE 0 END) AS IncorrectCount,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 2*Value ELSE 0 END) AS IncorrectValue,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN 1 ELSE 0 END) AS DNRCount,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN Value ELSE 0 END) AS DNRValue
 FROM jeopardy_data
-WHERE Metacategory = 'Words' AND Response = 'Incorrect'
-GROUP BY Game),
-wordsdnr AS (
-SELECT Game,
-	SUM(Value) AS dnrvalue
-FROM jeopardy_data
-WHERE Metacategory = 'Words' AND Response = 'Did Not Ring In'
-GROUP BY Game),
-wordslost AS (
-SELECT a.Game,
-	SUM(a.incvalue + b.dnrvalue) AS PotentialLostEarnings
-FROM wordsinc a
-JOIN wordsdnr b
-ON a.Game = b.Game
-GROUP BY a.Game)
-SELECT ROUND(AVG(PotentialLostEarnings),0)
-FROM wordslost;
+WHERE Metacategory = 'Words'
+GROUP BY Game)
+SELECT ROUND(SUM(IncorrectValue+DNRValue)/80,0) AS TotalLeft
+FROM MetaValuesWords
+;
 
 
+-- How many Words questions go unanswered or incorrectly answered per game?
+WITH MetaValuesWords AS (SELECT Game,
+	COUNT(Response) AS TotalResponse,
+	SUM(CASE WHEN Response = 'Correct' THEN 1 ELSE 0 END) AS CorrectCount,
+	SUM(CASE WHEN Response = 'Correct' THEN Value ELSE 0 END) AS CorrectValue,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 1 ELSE 0 END) AS IncorrectCount,
+	SUM(CASE WHEN Response = 'Incorrect' THEN 2*Value ELSE 0 END) AS IncorrectValue,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN 1 ELSE 0 END) AS DNRCount,
+	SUM(CASE WHEN Response = 'Did Not Ring In' THEN Value ELSE 0 END) AS DNRValue
+FROM jeopardy_data
+WHERE Metacategory = 'Words'
+GROUP BY Game)
+SELECT ROUND(SUM(IncorrectCount+DNRCount)/80,0) AS TotalLeft
+FROM MetaValuesWords
+;
